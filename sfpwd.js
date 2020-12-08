5ae7a65bdf02015ccdb7422905e8a395ce097aa0
MarceloHeredia <marcelo.heredia@acad.pucrs.br>
2020-12-08 13:13:07 -0300

    design almost ready, starting input validation. TODO: alert modal

--- sfpwd.js
+++ sfpwd.js
@@ -6,16 +6,23 @@ const alph_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const nums = '0123456789';
 const special_c = '!@#$%¨&*()_+-={}[]`´~^/?;:.,<>|';
 
-
 $(document).ready(function(){
-
-    $('#tst').click(function (){
-        testit();
-
+    //Generate Password
+    $('#gen_pwd').click(function (){
+        test_inputs();
     });
+
+    const test_inputs = () => {
+        if ($('#key_len').val() < 6 ||
+            $('#key_len').val() > 2048){
+            //alert error
+            return false;
+        }
+
+    }
 
     function testit(){
         a = parseInt(crypto.randomBytes(4).toString('hex'),16);
-        $('#result').text('dsfgra: '+ a);
+        $('#pwd_out').val(a);
     }
 });
